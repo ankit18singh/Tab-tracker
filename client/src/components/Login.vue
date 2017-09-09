f3aac61d72f07a1cbfd4979f117c70847435f5a7
ankit18singh <aj.ankitsingh@gmail.com>
2017-09-09 19:51:30 +0530

    login endpoint created and Vuex implemented

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,7 +3,7 @@
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
+          <v-toolbar-title>Login</v-toolbar-title>
         </v-toolbar>
 
           <div class="pl-4 pr-4 pt-2 pb-2"> 
@@ -26,8 +26,8 @@
             <div class= "error" v-html="error" />
             <v-btn
               class="cyan"
-              @click="register"
-              dark>Register</v-btn>
+              @click="login"
+              dark>Login</v-btn>
         </div>
       </div>
     </v-flex>
@@ -50,9 +50,9 @@ export default {
     }
   },
   methods: {
-    async register () {
+    async login () {
       try {
-        const response = await AuthenticationService.register({
+        const response = await AuthenticationService.login({
           email: this.email,
           password: this.password
         })
---
+++ client/src/components/PageHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-toolbar fixed class="cyan" dark>
+    <v-toolbar-title class="mr-4">
+      <span
+        class="home"
+        @click="navigateTo({name: 'hello'})">
+          TabTracker
+      </span>
+    </v-toolbar-title>
+
+    <v-toolbar-items>
+      <v-btn flat dark
+        @click="navigateTo({name: 'hello'})">
+        Browse
+      </v-btn>
+    </v-toolbar-items>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items
+      v-if="!$store.state.isUserLoggedIn">
+        <v-btn flat dark
+          @click="navigateTo({name: 'Login'})">
+            Login
+        </v-btn>
+    </v-toolbar-items>
+
+    <v-toolbar-items
+      v-if="!$store.state.isUserLoggedIn">
+        <v-btn flat dark
+          @click="navigateTo({name: 'Register'})">
+            Sign-Up
+        </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  cursor: pointer;
+}
+.home:hover {
+  color: black;
+}
+</style>
